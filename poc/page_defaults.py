9be84edeec65ca6fa60f3af9bcc6ae20cc47dadf
Guido Jiménez <enigemic@sounds>
2025-08-31 16:12:13 +0100

    Minor formatting changes

--- poc/page_defaults.py
+++ poc/page_defaults.py
@@ -2,25 +2,25 @@ pages = {
     'index.html': {
         'path': '/',
         'menu_display': 'home',
-        'page_name': "producer's alcove_:home",
+        'page_name': "producer's alcove_ : home",
         'page_subtitle': None
     },
     'search.html': {
         'path': '/search',
         'menu_display': 'shortcut search',
-        'page_name': "producer's alcove_:shortcut search",
+        'page_name': "producer's alcove_ : shortcut search",
         'page_subtitle': 'protools shortcut search'
     },
     'note2Hz.html': {
         'path': '/note2Hz',
         'menu_display': 'note to Hz calculator',
-        'page_name': "producer's alcove_:note to Hz",
+        'page_name': "producer's alcove_ : note to Hz",
         'page_subtitle': 'note to Hz calculator'
     },
     'note2ms.html': {
         'path': '/note2ms',
         'menu_display': 'note to ms calculator',
-        'page_name': "producer's alcove_:note to ms",
+        'page_name': "producer's alcove_ : note to ms",
         'page_subtitle': 'note to ms calculator'
     },
 }
@@ -46,6 +46,8 @@ class PageDefaults:
         for filename, data in pages.items():
             if filename == self.filename:
                 continue
-            ret.append({key_map.get(k, k): v for k, v in data.items()})
+            ret.append(
+                {key_map.get(k, k): v for k, v in data.items()}
+            )
 
         return ret
