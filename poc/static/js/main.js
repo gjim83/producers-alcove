7615bfd008cebcc66edcf7bdc96adc5abd5cbb97
Guido Jiménez <enigemic@sounds>
2025-09-01 15:34:04 +0100

    Adding easter egg code to search page

--- poc/static/js/main.js
+++ poc/static/js/main.js
@@ -1,3 +1,25 @@
 function menuClick(x) {
-  x.classList.toggle("change");
+    x.classList.toggle("change");
+}
+
+// search page easter egg
+let ip = '';
+let popupCount = 0;
+function easterEgg() {
+    if (ip !== '62.31.28.217') { return true; };
+    if (popupCount < 2) {
+        var popup = document.getElementById("easterEggPopup");
+        popup.classList.toggle("show");
+        popupCount++;
+  };
+}
+
+async function getIP () {
+    try {
+        const response = await fetch('https://api.ipify.org?format=json');
+        const data = await response.json();
+        ip = data.ip;
+    } catch (error) {
+        console.error('Error fetching IP address:', error);
+    }
 }
